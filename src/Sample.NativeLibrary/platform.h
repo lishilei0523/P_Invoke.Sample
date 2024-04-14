3f4d592bdd7565bbbf0305f419276010c5639091
Lee <lishilei0523@163.com>
2024-04-14 23:54:21 +0800

    -

--- src/Sample.NativeLibrary/platform.h
+++ src/Sample.NativeLibrary/platform.h
@@ -1,7 +1,7 @@
 #pragma once
-#define EXTERN_C extern "C" __declspec(dllexport)
+#define EXPORT extern "C" __declspec(dllexport)
 #define CALLING_MODE __stdcall
 
-EXTERN_C int CALLING_MODE greet(const char* text);
-EXTERN_C int CALLING_MODE sum(const int a, const int b);
-EXTERN_C int CALLING_MODE minus(const int a, const int b);
+EXPORT int CALLING_MODE greet(const char* text);
+EXPORT int CALLING_MODE sum(const int a, const int b);
+EXPORT int CALLING_MODE minus(const int a, const int b);
